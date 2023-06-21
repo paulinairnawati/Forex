815f902e70510753b00a277d7071babd4684e9c3
Forex <paulinairnawati83@gmail.com>
2023-06-21 13:58:13 +0700

    Forex

---
+++ app/src/main/java/com/paulinairnawati201103641/forex/RootModel.java
@@ -0,0 +1,14 @@
+package com.paulinairnawati201103641.forex;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RootModel {
+    @SerializedName("rates")
+    private RatesModel ratesModel;
+
+    public RootModel() {}
+
+    public RatesModel getRatesModel() { return ratesModel; }
+
+    public void setRatesModel(RatesModel ratesModel) {this.ratesModel = ratesModel; }
+}
